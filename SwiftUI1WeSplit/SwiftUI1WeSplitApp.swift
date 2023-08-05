1599f4062bcb436d17b49556e176833bbc0a737c
Nazarzbs <famina1998@gmail.com>
2023-08-05 14:38:28 +0300

    Initial Commit

---
+++ SwiftUI1WeSplit/SwiftUI1WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI1WeSplitApp.swift
+//  SwiftUI1WeSplit
+//
+//  Created by Nazar on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI1WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
